a79d599f0105cab22e26eee1e77c3dc1f74bc3a8
Jed Watson <jed.watson@me.com>
2015-05-12 18:41:25 +1000

    Fixing default user details

--- app/templates/updates/0.0.1-admins.js
+++ app/templates/updates/0.0.1-admins.js
@@ -1,5 +1,5 @@
 exports.create = {
 	User: [
-		{ 'name.full': 'Admin User', email: 'user@thinkmill.com.au', password: '51ec9b31-0e34-49c6-a16b-ec794c8a49bc', isAdmin: true }
+		{ 'name.full': 'Admin User', email: 'user@keystonejs.com', password: 'admin', isAdmin: true }
 	]
 };
