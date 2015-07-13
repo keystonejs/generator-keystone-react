b7487154cfaf18a37c703780293307d7439cd49a
Jed Watson <jed.watson@me.com>
2015-07-13 23:54:03 +1000

    These API endpoints should not be in here

--- app/templates/routes/index.js
+++ app/templates/routes/index.js
@@ -1,7 +1,7 @@
 var babelify = require('babelify');
 var browserify = require('browserify-middleware');
 var keystone = require('keystone');
-	
+
 var importRoutes = keystone.importer(__dirname);
 var routes = {
 	api: importRoutes('./api')
@@ -9,14 +9,10 @@ var routes = {
 
 // Setup Route Bindings
 exports = module.exports = function(app) {
-	
+
 	app.use('/js', browserify('./client/scripts', {
 		transform: [babelify]
 	}));
-
-	// API
-	app.get('/api/clients', routes.api.client);
-	app.get('/api/providers', routes.api.provider);
 
 	// Views
 	app.use(function(req, res) {
