fb7fde532cff255d2be0407cdf869841f8a35e4f
Jed Watson <jed.watson@me.com>
2015-12-09 18:04:51 +1100

    These spaces should be tabs

--- app/index.js
+++ app/index.js
@@ -7,96 +7,96 @@ var yeoman = require('yeoman-generator');
 
 var KeystoneGenerator = yeoman.generators.Base.extend({
 
-  initializing: function() {
-    this.pkg = require('../package.json');
-  },
+	initializing: function() {
+		this.pkg = require('../package.json');
+	},
 
-  prompting: function() {
-    var done = this.async();
+	prompting: function() {
+		var done = this.async();
 
-    this.log('\nWelcome to the ' + chalk.green('Keystone React') + ' generator!\n');
+		this.log('\nWelcome to the ' + chalk.green('Keystone React') + ' generator!\n');
 
-    var prompts = [{
-      type: 'input',
-      name: 'projectName',
-      message: 'What is the name of your project?',
-      default: 'My Site'
-    }, {
-      type: 'confirm',
-      name: 'createDirectory',
-      message: 'Would you like to create a new directory for your project?',
-      default: true
-    }];
+		var prompts = [{
+			type: 'input',
+			name: 'projectName',
+			message: 'What is the name of your project?',
+			default: 'My Site'
+		}, {
+			type: 'confirm',
+			name: 'createDirectory',
+			message: 'Would you like to create a new directory for your project?',
+			default: true
+		}];
 
-    this.prompt(prompts, function(props) {
-      this.log('\n');
-      _.extend(this, props);
-      this.projectKey = utils.slug(this.projectName);
-      if (props.createDirectory) {
-        this.destinationRoot(this.projectKey);
-      }
-      done();
-    }.bind(this));
-  },
+		this.prompt(prompts, function(props) {
+			this.log('\n');
+			_.extend(this, props);
+			this.projectKey = utils.slug(this.projectName);
+			if (props.createDirectory) {
+				this.destinationRoot(this.projectKey);
+			}
+			done();
+		}.bind(this));
+	},
 
-  keys: function keys() {
-    this.cookieSecret = crypto.randomBytes(64).toString('hex');
-  },
+	keys: function keys() {
+		this.cookieSecret = crypto.randomBytes(64).toString('hex');
+	},
 
-  writing: {
-    project: function() {
-      this.copy('keystone.js', 'keystone.js');
-      this.copy('editorconfig', '.editorconfig');
-      this.copy('gitignore', '.gitignore');
-      this.copy('jshintrc', '.jshintrc');
-      this.copy('Procfile', 'Procfile');
-      this.template('_package.json', 'package.json');
-    },
+	writing: {
+		project: function() {
+			this.copy('keystone.js', 'keystone.js');
+			this.copy('editorconfig', '.editorconfig');
+			this.copy('gitignore', '.gitignore');
+			this.copy('jshintrc', '.jshintrc');
+			this.copy('Procfile', 'Procfile');
+			this.template('_package.json', 'package.json');
+		},
 
-    clientfiles: function() {
-      this.copy('client/scripts/application.js', 'client/scripts/application.js');
-    },
+		clientfiles: function() {
+			this.copy('client/scripts/application.js', 'client/scripts/application.js');
+		},
 
-    modelfiles: function() {
-      this.copy('models/User.js', 'models/User.js');
-    },
+		modelfiles: function() {
+			this.copy('models/User.js', 'models/User.js');
+		},
 
-    publicfiles: function() {
-      this.copy('public/favicon.ico', 'public/favicon.ico');
-      this.directory('public/styles', 'public/styles');
-    },
+		publicfiles: function() {
+			this.copy('public/favicon.ico', 'public/favicon.ico');
+			this.directory('public/styles', 'public/styles');
+		},
 
-    routesfiles: function() {
-      this.directory('routes/api', 'routes/api');
-      this.copy('routes/index.js', 'routes/index.js');
-    },
+		routesfiles: function() {
+			this.directory('routes/api', 'routes/api');
+			this.copy('routes/index.js', 'routes/index.js');
+		},
 
-    templatefiles: function() {
-      this.copy('templates/views/index.jade', 'templates/views/index.jade');
-    },
+		templatefiles: function() {
+			this.copy('templates/views/index.jade', 'templates/views/index.jade');
+		},
 
-    updatefiles: function() {
-      this.copy('updates/0.0.1-admins.js', 'updates/0.0.1-admins.js');
-    }
+		updatefiles: function() {
+			this.copy('updates/0.0.1-admins.js', 'updates/0.0.1-admins.js');
+		}
 
-  },
+	},
 
-  install: function() {
-    this.log('\n' + chalk.green('Running npm install...') +
-    '\n'
-    );
-    this.npmInstall();
-  },
+	install: function() {
+		this.log('\n' + chalk.green('Running npm install...') +
+		'\n'
+		);
+		this.npmInstall();
+	},
 
-  end: function() {
-    var cmd = (this.createDirectory ? '"cd ' + utils.slug(this.projectName) + '" then ' : '') + '"node keystone"';
-    this.log(
-      '\n' + chalk.green.underline('Your new project is ready!') +
-      '\n' +
-      '\n\nTo start Keystone, run ' + cmd + '.' +
-      '\n'
-    );
-  }
+	end: function() {
+		var cmd = (this.createDirectory ? '"cd ' + utils.slug(this.projectName) + '" then ' : '') + '"node keystone"';
+		this.log(
+			'\n' + chalk.green.underline('Your new project is ready!') +
+			'\n' +
+			'\n\nTo start Keystone, run ' + cmd + '.' +
+			'\n'
+		);
+	}
 });
 
 module.exports = KeystoneGenerator;
